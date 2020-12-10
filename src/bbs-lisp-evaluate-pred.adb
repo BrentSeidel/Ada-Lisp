0701746bf10e3256766815aba796285176cddfce
BrentSeidel <brentseidel@mac.com>
2020-12-10 10:23:12 -0700

    Documentation updates. Reorder functions in src/bbs-lisp-evaluate-pred.adb to alphabetical order to make it easier to find them.

--- src/bbs-lisp-evaluate-pred.adb
+++ src/bbs-lisp-evaluate-pred.adb
@@ -8,39 +8,6 @@ package body BBS.lisp.evaluate.pred is
    --
    --  These return true of false depending on the type of data passed.
    --
-   function nullp(e : element_type) return element_type is
-      t : element_type := e;
-      p : element_type;
-   begin
-      if not isList(e) then
-         error("nullp", "Internal error, not passed a list.");
-         return (kind => E_ERROR);
-      end if;
-      p := first_value(t);
-      if p = NIL_ELEM then
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-      else
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
-      end if;
-   end;
-   --
-   function symbolp(e : element_type) return element_type is
-      p : element_type;
-      s : cons_index;
-   begin
-      if not isList(e) then
-         error("symbolp", "Internal error, not passed a list.");
-         return (kind => E_ERROR);
-      end if;
-      s := getList(e);
-      p := cons_table(s).car;
-      if p.kind = E_SYMBOL then
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-      else
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
-      end if;
-   end;
-   --
    function atomp(e : element_type) return element_type is
       p : element_type;
       s : cons_index;
@@ -52,88 +19,6 @@ package body BBS.lisp.evaluate.pred is
       s := getList(e);
       p := cons_table(s).car;
       if not isList(p) then
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-      else
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
-      end if;
-   end;
-   --
-   function consp(e : element_type) return element_type is
-      p : element_type;
-      s : cons_index;
-   begin
-      if not isList(e) then
-         error("symbolp", "Internal error, not passed a list.");
-         return (kind => E_ERROR);
-      end if;
-      s := getList(e);
-      p := cons_table(s).car;
-      if isList(p) then
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-      else
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
-      end if;
-   end;
-   --
-   function listp(e : element_type) return element_type is
-      p : element_type;
-      s : cons_index;
-   begin
-      if not isList(e) then
-         error("symbolp", "Internal error, not passed a list.");
-         return (kind => E_ERROR);
-      end if;
-      s := getList(e);
-      p := cons_table(s).car;
-      if isList(p) or (p = NIL_ELEM) then
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-      else
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
-      end if;
-   end;
-   --
-   function numberp(e : element_type) return element_type is
-      t : element_type := e;
-      p : element_type;
-   begin
-      if not isList(e) then
-         error("nullp", "Internal error, not passed a list.");
-         return (kind => E_ERROR);
-      end if;
-      p := first_value(t);
-      if (p.kind = E_VALUE) and then (p.v.kind = V_INTEGER) then
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-      else
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
-      end if;
-   end;
-   --
-   function integerp(e : element_type) return element_type is
-      t : element_type := e;
-      p : element_type;
-   begin
-      if not isList(e) then
-         error("nullp", "Internal error, not passed a list.");
-         return (kind => E_ERROR);
-      end if;
-      p := first_value(t);
-      if (p.kind = E_VALUE) and then (p.v.kind = V_INTEGER) then
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-      else
-         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
-      end if;
-   end;
-   --
-   function stringp(e : element_type) return element_type is
-      t : element_type := e;
-      p : element_type;
-   begin
-      if not isList(e) then
-         error("nullp", "Internal error, not passed a list.");
-         return (kind => E_ERROR);
-      end if;
-      p := first_value(t);
-      if (p.kind = E_VALUE) and then (p.v.kind = V_STRING) then
          return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
       else
          return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
@@ -156,7 +41,7 @@ package body BBS.lisp.evaluate.pred is
       end if;
    end;
    --
-   function simple_string_p(e : element_type) return element_type is
+   function compiled_function_p(e : element_type) return element_type is
       t : element_type := e;
       p : element_type;
    begin
@@ -165,7 +50,26 @@ package body BBS.lisp.evaluate.pred is
          return (kind => E_ERROR);
       end if;
       p := first_value(t);
-      if (p.kind = E_VALUE) and then (p.v.kind = V_STRING) then
+      if p.kind = E_SYMBOL then
+         if (symb_table(p.sym).kind = SY_BUILTIN) or
+           (symb_table(p.sym).kind = SY_SPECIAL) then
+            return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+         end if;
+      end if;
+      return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+   end;
+   --
+   function consp(e : element_type) return element_type is
+      p : element_type;
+      s : cons_index;
+   begin
+      if not isList(e) then
+         error("symbolp", "Internal error, not passed a list.");
+         return (kind => E_ERROR);
+      end if;
+      s := getList(e);
+      p := cons_table(s).car;
+      if isList(p) then
          return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
       else
          return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
@@ -196,7 +100,7 @@ package body BBS.lisp.evaluate.pred is
       return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
    end;
    --
-   function compiled_function_p(e : element_type) return element_type is
+   function integerp(e : element_type) return element_type is
       t : element_type := e;
       p : element_type;
    begin
@@ -205,13 +109,109 @@ package body BBS.lisp.evaluate.pred is
          return (kind => E_ERROR);
       end if;
       p := first_value(t);
-      if p.kind = E_SYMBOL then
-         if (symb_table(p.sym).kind = SY_BUILTIN) or
-           (symb_table(p.sym).kind = SY_SPECIAL) then
-            return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
-         end if;
+      if (p.kind = E_VALUE) and then (p.v.kind = V_INTEGER) then
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+      else
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
       end if;
-      return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+   end;
+   --
+   function listp(e : element_type) return element_type is
+      p : element_type;
+      s : cons_index;
+   begin
+      if not isList(e) then
+         error("symbolp", "Internal error, not passed a list.");
+         return (kind => E_ERROR);
+      end if;
+      s := getList(e);
+      p := cons_table(s).car;
+      if isList(p) or (p = NIL_ELEM) then
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+      else
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+      end if;
+   end;
+   --
+   function nullp(e : element_type) return element_type is
+      t : element_type := e;
+      p : element_type;
+   begin
+      if not isList(e) then
+         error("nullp", "Internal error, not passed a list.");
+         return (kind => E_ERROR);
+      end if;
+      p := first_value(t);
+      if p = NIL_ELEM then
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+      else
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+      end if;
+   end;
+   --
+   function numberp(e : element_type) return element_type is
+      t : element_type := e;
+      p : element_type;
+   begin
+      if not isList(e) then
+         error("nullp", "Internal error, not passed a list.");
+         return (kind => E_ERROR);
+      end if;
+      p := first_value(t);
+      if (p.kind = E_VALUE) and then (p.v.kind = V_INTEGER) then
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+      else
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+      end if;
+   end;
+   --
+   function simple_string_p(e : element_type) return element_type is
+      t : element_type := e;
+      p : element_type;
+   begin
+      if not isList(e) then
+         error("nullp", "Internal error, not passed a list.");
+         return (kind => E_ERROR);
+      end if;
+      p := first_value(t);
+      if (p.kind = E_VALUE) and then (p.v.kind = V_STRING) then
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+      else
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+      end if;
+   end;
+   --
+   function stringp(e : element_type) return element_type is
+      t : element_type := e;
+      p : element_type;
+   begin
+      if not isList(e) then
+         error("nullp", "Internal error, not passed a list.");
+         return (kind => E_ERROR);
+      end if;
+      p := first_value(t);
+      if (p.kind = E_VALUE) and then (p.v.kind = V_STRING) then
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+      else
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+      end if;
+   end;
+   --
+   function symbolp(e : element_type) return element_type is
+      p : element_type;
+      s : cons_index;
+   begin
+      if not isList(e) then
+         error("symbolp", "Internal error, not passed a list.");
+         return (kind => E_ERROR);
+      end if;
+      s := getList(e);
+      p := cons_table(s).car;
+      if p.kind = E_SYMBOL then
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => True));
+      else
+         return (kind => E_VALUE, v => (kind => V_BOOLEAN, b => False));
+      end if;
    end;
    --
    --  These always return false as the data types are not implemented.  There's
