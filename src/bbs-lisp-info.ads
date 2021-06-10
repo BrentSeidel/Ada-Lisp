f37c86a2330007fa4174923b50b5edf6ac0cd5a1
BrentSeidel <brentseidel@mac.com>
2021-06-10 10:57:36 -0700

    Add some more test cases and update code coverage.

--- src/bbs-lisp-info.ads
+++ src/bbs-lisp-info.ads
@@ -4,9 +4,9 @@ package BBS.lisp.info is
    --  the test coverage script.  It should not be edited by hand.
    --
    name           : constant String  := "Tiny Lisp";
-   timestamp      : constant String  := "Wed Jun  9 15:28:00 MST 2021";
-   build_date     : constant String  := "2021-Jun-09";
+   timestamp      : constant String  := "Thu Jun 10 10:57:36 MST 2021";
+   build_date     : constant String  := "2021-Jun-10";
    version_string : constant String  := "V00.02+";
-   version_date   : constant Integer := 20210609;  -- yyyymmdd
+   version_date   : constant Integer := 20210610;  -- yyyymmdd
    version_number : constant Integer := 2;
 end;
