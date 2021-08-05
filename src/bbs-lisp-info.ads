06efd225ff31a33a73fc90b1026bf9e2bd3089b4
BrentSeidel <brentseidel@mac.com>
2021-08-05 12:50:25 -0700

    Update documentation per issue #39.

--- src/bbs-lisp-info.ads
+++ src/bbs-lisp-info.ads
@@ -4,7 +4,7 @@ package BBS.lisp.info is
    --  the test coverage script.  It should not be edited by hand.
    --
    name           : constant String  := "Tiny Lisp";
-   timestamp      : constant String  := "Thu Aug  5 10:07:26 MST 2021";
+   timestamp      : constant String  := "Thu Aug  5 12:50:26 MST 2021";
    build_date     : constant String  := "2021-Aug-05 (master)";
    version_string : constant String  := "V00.03+";
    version_date   : constant Integer := 20210805;  -- yyyymmdd
