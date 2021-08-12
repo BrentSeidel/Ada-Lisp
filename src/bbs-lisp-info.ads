2bdb9fdd5c016c3944546ca73fa33bc5d9372209
BrentSeidel <brentseidel@mac.com>
2021-08-12 15:11:54 -0700

    Update version to V00.04+ to start work on the next version. Change string names for error codes to use the enumeration 'Image attribute.

--- src/bbs-lisp-info.ads
+++ src/bbs-lisp-info.ads
@@ -4,9 +4,9 @@ package BBS.lisp.info is
    --  the test coverage script.  It should not be edited by hand.
    --
    name           : constant String  := "Tiny Lisp";
-   timestamp      : constant String  := "Sun Aug  8 11:54:15 MST 2021";
-   build_date     : constant String  := "2021-Aug-08 (master)";
-   version_string : constant String  := "V00.04";
-   version_date   : constant Integer := 20210808;  -- yyyymmdd
+   timestamp      : constant String  := "Thu Aug 12 15:11:54 MST 2021";
+   build_date     : constant String  := "2021-Aug-12 (master)";
+   version_string : constant String  := "V00.04+";
+   version_date   : constant Integer := 20210812;  -- yyyymmdd
    version_number : constant Integer := 2;
 end;
