6c106ef49b47c4b0146bab5df747ba1030d9b28b
BrentSeidel <brentseidel@mac.com>
2023-12-17 10:17:39 -0700

    Minor update to lispq.gpr.

--- src/bbs-lisp-info.ads
+++ src/bbs-lisp-info.ads
@@ -4,9 +4,9 @@ package BBS.lisp.info is
    --  the test coverage script.  It should not be edited by hand.
    --
    name           : constant String  := "Tiny Lisp";
-   timestamp      : constant String  := "Tue Aug 31 12:32:11 MST 2021";
-   build_date     : constant String  := "2021-Aug-31 (master)";
+   timestamp      : constant String  := "Sun Dec 17 10:17:40 MST 2023";
+   build_date     : constant String  := "2023-Dec-17 (master)";
    version_string : constant String  := "V00.04+";
-   version_date   : constant Integer := 20210831;  -- yyyymmdd
+   version_date   : constant Integer := 20231217;  -- yyyymmdd
    version_number : constant Integer := 2;
 end;
