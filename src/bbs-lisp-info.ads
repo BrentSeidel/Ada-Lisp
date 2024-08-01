70291078f58dc05d7be252a5655e3c02683081e8
BrentSeidel <brentseidel@mac.com>
2024-08-01 13:15:46 -0700

    Update info file.

--- src/bbs-lisp-info.ads
+++ src/bbs-lisp-info.ads
@@ -4,9 +4,9 @@ package BBS.lisp.info is
    --  the test coverage script.  It should not be edited by hand.
    --
    name           : constant String  := "Tiny Lisp";
-   timestamp      : constant String  := "Wed Jul 31 13:44:22 MST 2024";
-   build_date     : constant String  := "2024-Jul-31 (master)";
+   timestamp      : constant String  := "Thu Aug  1 13:15:18 MST 2024";
+   build_date     : constant String  := "2024-Aug-01 (master)";
    version_string : constant String  := "V00.04+";
-   version_date   : constant Integer := 20240731;  -- yyyymmdd
+   version_date   : constant Integer := 20240801;  -- yyyymmdd
    version_number : constant Integer := 2;
 end;
