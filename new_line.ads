053250e9c3db4215eb030e794bffd94ed887355f
BrentSeidel <brentseidel@mac.com>
2024-07-31 18:59:00 -0700

    Add some missing copyright headers.

--- new_line.ads
+++ new_line.ads
@@ -1,4 +1,21 @@
 --
+--  Author: Brent Seidel
+--  Date: 31-Jul-2024
+--
+--  This file is part of Tiny-Lisp CLI.
+--  Tiny-Lisp is free software: you can redistribute it and/or modify it
+--  under the terms of the GNU General Public License as published by the
+--  Free Software Foundation, either version 3 of the License, or (at your
+--  option) any later version.
+--
+--  Tiny-Lisp is distributed in the hope that it will be useful, but
+--  WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+--  Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License along
+--  with Tiny-Lisp. If not, see <https://www.gnu.org/licenses/>.--
+--
 --  The text_io version of newline contains an optional parameter indicating
 --  the number of lines to skip.  The type of this parameter is defined in
 --  Ada.Text_IO.  This makes it awkward to define a function prototype that can
